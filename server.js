3ca186d6fe76f456d05035a047e8d5110b52d0cc
Hawraa-Nasser <hawranaser34@gmail.com>
2026-05-08 13:56:22 +0300

    Add route to serve index.html file
    fix Cannot GET route

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // In-memory array for messages
 const messages = [];
